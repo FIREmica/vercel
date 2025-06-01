00a4fc7cb0a56349bc13cfa48fef63428bcdadf5
Ronald González <akuma424424@gmail.com>
2025-06-01 10:36:01 +0000

    Para configurar una aplicación Next.js para permitir solicitudes de orígenes distintos del nombre de host, se inicializó el servidor (localhost por defecto) puedes usar el allowedDevOrigins opción de configuración.
    allowedDevOrigins le permite establecer orígenes adicionales que se pueden utilizar en el modo de desarrollo. Por ejemplo, para usar local-origin.dev en lugar de solo localhost, abierto next.config.js y añadir el allowedDevOrigins configurar:
    
    siguiente.config.js
    
    module.exports = {
      allowedDevOrigins: ['local-origin.dev', '*.local-origin.dev'],
    }
    Anterior
    siguiente.config.js
    Siguiente

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,4 @@
+
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
@@ -18,6 +19,7 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  allowedDevOrigins: ['local-origin.dev', '*.local-origin.dev'],
 };
 
 export default nextConfig;
